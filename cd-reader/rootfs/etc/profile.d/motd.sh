d0390e57cf36d83bb6ae12b5fb6dd4b6b16ddb4a
Eddie Webbinaro <ollitech@gmail.com>
2022-11-22 18:46:12 -0500

    1.1.12 (cleanup chomd from 1.1.11 which worked)

--- cd-reader/rootfs/etc/profile.d/motd.sh
+++ cd-reader/rootfs/etc/profile.d/motd.sh
@@ -1,7 +1,9 @@
 #!/command/with-contenv bashio
 
+echo -e "MP4 Folder: $(bashio::config 'mp4_path')\n"
 
-[ "$(id -u)" -eq 0 ] && echo -e 'MP4 Folder: $(bashio::config "mp4_path")\n
+[ "$(id -u)" -eq 0 ] && \
+echo -e '
 USAGE: This add-on supports CD and DVD Backups. \n
 TO BACKUP CDs: Insert CD and run `cd` to fire `abcde` with default values and writing to MP3 directory.\n
 TO BACKUP DVDs: Insert DVD and run `dvd` to fire `HandBrakeCLI` with default values and writing to Movies directory.\n
