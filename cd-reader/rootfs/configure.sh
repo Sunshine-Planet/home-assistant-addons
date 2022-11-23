b5d0a120ad0a3a35f640347448c3fa974c2ef729
Eddie Webbinaro <ollitech@gmail.com>
2022-11-22 19:48:29 -0500

    Phoned it in

--- cd-reader/rootfs/configure.sh
+++ cd-reader/rootfs/configure.sh
@@ -3,4 +3,4 @@ echo "export MP3_PATH=$(bashio::config 'mp3_path')" >> /etc/profile.d/config.sh
 echo "export MP4_PATH=$(bashio::config 'mp4_path')"  >> /etc/profile.d/config.sh
 chmod a+x /etc/profile.d/config.sh
 
-echo "OUTPUTDIR=$MP3_PATH" >> /etc/abcde.conf
+echo "OUTPUTDIR=$(bashio::config 'mp3_path')"  >> /etc/abcde.conf
