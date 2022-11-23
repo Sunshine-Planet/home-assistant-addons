947b50c15e8ef08d93c7bc153b819e71c08b86d0
Eddie Webbinaro <ollitech@gmail.com>
2022-11-22 19:21:07 -0500

    1.0.15

--- cd-reader/rootfs/etc/profile.d/alias.sh
+++ cd-reader/rootfs/etc/profile.d/alias.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bashio
 
+# #this file witll be whatever shell user has, but we invoke bashio shell for config
 source /configure.sh
 
 function dvd() {
     echo "will write movie to: $MP4_PATH"
-    TITLE=`HandBrakeCLI -I -i /dev/sr0 -o /dev/null/garabe.mp4 2>&1 | grep 'DVD Title:' | head -1 | rev | cut -f1 -d" " | rev | tr "_" " "`
+    TITLE=`HandBrakeCLI -I -i /dev/sr0 -o /dev/null/garabe.mp4 2>&1 | grep 'DVD Title:' | head -1 | rev | cut -f1 -d":" | rev | tr "_" " "`
     HandBrakeCLI --main-feature -i /dev/sr0 -o "$MP4_PATH/${TITLE}.mp4" -q 20 -B 160 -s none
     echo "wrote movie to: $MP4_PATH/${TITLE}.mp4"
 }
