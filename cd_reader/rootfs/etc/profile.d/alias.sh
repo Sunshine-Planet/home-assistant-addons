f3050bb03d3df522004dbdca41e3ed9e57ac4a67
Eddie Webbinaro <ollitech@gmail.com>
2023-12-28 18:26:04 -0500

    fix bug in title count

--- cd_reader/rootfs/etc/profile.d/alias.sh
+++ cd_reader/rootfs/etc/profile.d/alias.sh
@@ -15,7 +15,6 @@ function mp3() {
     abcde -o mp3:-V2 -x -p
 }
 
-
 function series () 
 { 
     MYID=$BASHPID #avoid subshell issues
@@ -26,8 +25,9 @@ function series ()
     HandBrakeCLI -i /dev/sr0 --title=0 --min-duration $min_dur > /tmp/stdout_$MYID.txt 2>/tmp/stderr_$MYID.txt
     TITLE=`cat /tmp/stdout_$MYID.txt | grep 'DVD Title:' | head -1 | rev | cut -f1 -d":" | rev | tr "_" " "`;
     echo "Using Title Prefix $TITLE"    
-    TITLES=` cat /tmp/stderr_$MYID.txt | grep -e '+ title [0-9]*' | sed 's/+ title \([0-9]*\):/\1/'`
-    echo "Found $(echo ${TITLES[@]}|wc -l) titles over $minutes minutes (you can change this by passing minues to command)"
+    TITLES=`cat /tmp/stderr_$MYID.txt | grep -e '+ title [0-9]*' | sed 's/+ title \([0-9]*\):/\1/'`
+    TITLES=($TITLES)
+    echo "Found ${#TITLES[@]} titles over $minutes minutes (you can change this by passing minues to command)"
     for index in ${TITLES[@]};do
         echo "writing title ${index} to $MP4_PATH/${TITLE}-${index}.mp4  ...."
         HandBrakeCLI --title ${index} -i /dev/sr0 -o "$MP4_PATH/${TITLE}-${index}.mp4"  -q 20 -B 160 -s none 2>/tmp/encode_$MYID.txt;
