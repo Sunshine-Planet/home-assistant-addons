10f20a6f832ffe83aac3dc8389d2d922806edcf0
Eddie Webbinaro <ollitech@gmail.com>
2023-12-28 18:01:26 -0500

    add series support for dvds

--- cd_reader/rootfs/etc/profile.d/alias.sh
+++ cd_reader/rootfs/etc/profile.d/alias.sh
@@ -15,4 +15,27 @@ function mp3() {
     abcde -o mp3:-V2 -x -p
 }
 
+
+function series () 
+{ 
+    MYID=$BASHPID #avoid subshell issues
+    minutes=${1:-21} #default: 30 minute shows with ads
+    min_dur=$(($minutes * 60))
+    echo "will write titles >${minutes}m to: $MP4_PATH";
+    echo "Gettting disc info..."
+    HandBrakeCLI -i /dev/sr0 --title=0 --min-duration $min_dur > /tmp/stdout_$MYID.txt 2>/tmp/stderr_$MYID.txt
+    TITLE=`cat /tmp/stdout_$MYID.txt | grep 'DVD Title:' | head -1 | rev | cut -f1 -d":" | rev | tr "_" " "`;
+    echo "Using Title Prefix $TITLE"    
+    TITLES=` cat /tmp/stderr_$MYID.txt | grep -e '+ title [0-9]*' | sed 's/+ title \([0-9]*\):/\1/'`
+    echo "Found $(echo ${TITLES[@]}|wc -l) titles over $minutes minutes (you can change this by passing minues to command)"
+    for index in ${TITLES[@]};do
+        echo "writing title ${index} to $MP4_PATH/${TITLE}-${index}.mp4  ...."
+        HandBrakeCLI --title ${index} -i /dev/sr0 -o "$MP4_PATH/${TITLE}-${index}.mp4"  -q 20 -B 160 -s none 2>/tmp/encode_$MYID.txt;
+        echo -e "\tdone"  
+    done
+    #
+    #echo "wrote movie to: $MP4_PATH/${TITLE}.mp4"
+}
+
 alias mp4='dvd'
+alias season='series'
