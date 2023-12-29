b0787f336f21f26e2c02170e558493469fe34c10
Eddie Webbinaro <ollitech@gmail.com>
2023-12-29 13:11:40 -0500

    naming convention for series

--- cd_reader/rootfs/etc/profile.d/alias.sh
+++ cd_reader/rootfs/etc/profile.d/alias.sh
@@ -5,7 +5,7 @@ bashio /configure.sh
 
 function dvd() {
     echo "will write movie to: $MP4_PATH"
-    TITLE=`HandBrakeCLI -I -i /dev/sr0 -o /dev/null/garabe.mp4 2>&1 | grep 'DVD Title:' | head -1 | rev | cut -f1 -d":" | rev | tr "_" " "`
+    TITLE=`HandBrakeCLI -I -i /dev/sr0 -o /dev/null/garabe.mp4 2>&1 | grep 'DVD Title:' | head -1 | rev| xargs | cut -f1 -d":" | rev | tr "_" " "`
     HandBrakeCLI --main-feature -i /dev/sr0 -o "$MP4_PATH/${TITLE}.mp4" -q 20 -B 160 -s none
     echo "wrote movie to: $MP4_PATH/${TITLE}.mp4"
 }
@@ -17,25 +17,34 @@ function mp3() {
 
 function series () 
 { 
+    echo "Usages: series SEASON DISC_FIRST_EPISODE MIN_RUN_MINUTES"
+    SEASON=${1:-1}
+    DISC_FIRST_EPISODE=${2:-1}
     MYID=$BASHPID #avoid subshell issues
-    minutes=${1:-21} #default: 30 minute shows with ads
+    minutes=${3:-21} #default: 30 minute shows with ads
     min_dur=$(($minutes * 60))
     echo "will write titles >${minutes}m to: $SHOWS_PATH";
     echo "Gettting disc info..."
     HandBrakeCLI -i /dev/sr0 --title=0 --min-duration $min_dur > /tmp/stdout_$MYID.txt 2>/tmp/stderr_$MYID.txt
-    TITLE=`cat /tmp/stdout_$MYID.txt | grep 'DVD Title:' | head -1 | rev | cut -f1 -d":" | rev | tr "_" " "`;
+    TITLE=`cat /tmp/stdout_$MYID.txt | grep 'DVD Title:' | head -1 | rev | cut -f1 -d":" | rev | xargs`;
     echo "Using Title Prefix $TITLE"    
+    if [ -z "$TITLE" ];then
+        echo "Error in script"
+    fi
     TITLES=`cat /tmp/stderr_$MYID.txt | grep -e '+ title [0-9]*' | sed 's/+ title \([0-9]*\):/\1/'`
     TITLES=($TITLES)
     echo "Found ${#TITLES[@]} titles over $minutes minutes (you can change this by passing minues to command)"
+    mkdir "$SHOWS_PATH/${TITLE%_*}"
     for index in ${TITLES[@]};do
-        echo "writing title ${index} to $SHOWS_PATH/${TITLE}-${index}.mp4  ...."
-        if [ -f "$SHOWS_PATH/${TITLE}-${index}.mp4" ];then
+        TARG="$SHOWS_PATH/${TITLE%_*}/${TITLE%_*} S${SEASON}E$((${DISC_FIRST_EPISODE}-1+${index})).mp4"
+
+        echo "writing title ${index} to  $TARG ...."
+        if [ -f "$TARG" ];then
             echo "file exists, skip"
             continue
         fi
         echo "please wait.."
-        HandBrakeCLI --title ${index} -i /dev/sr0 -o "$SHOWS_PATH/${TITLE}-${index}.mp4"  -q 20 -B 160 -s none 2>/tmp/encode_$MYID.txt >&2;
+        HandBrakeCLI --title ${index} -i /dev/sr0 -o "$TARG"  -q 20 -B 160 -s none 2>/tmp/encode_$MYID.txt >&2;
         echo -e "\tdone"  
     done
     #
