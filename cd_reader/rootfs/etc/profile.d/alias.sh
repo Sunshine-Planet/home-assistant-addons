d8034134efc0e1024e3b08be5b3fba320b5f3796
Eddie Webbinaro <ollitech@gmail.com>
2024-01-06 13:25:39 -0500

    improve season numbering

--- cd_reader/rootfs/etc/profile.d/alias.sh
+++ cd_reader/rootfs/etc/profile.d/alias.sh
@@ -18,16 +18,16 @@ function mp3() {
 function series () 
 { 
     SHOWS_PATH=${SERIES_PATH:-"/media/shows"}
-    echo "Usages: series SEASON DISC_FIRST_EPISODE MIN_RUN_MINUTES"
-    SEASON=${1:-1}
-    DISC_FIRST_EPISODE=${2:-1}
+    echo "Usages: series NAME SEASON DISC_FIRST_EPISODE [MIN_RUN_MINUTES]"
+    TITLE=${1}
+    SEASON=${2:-1}
+    DISC_FIRST_EPISODE=${3:-1}
     MYID=$BASHPID #avoid subshell issues
-    minutes=${3:-21} #default: 30 minute shows with ads
+    minutes=${4:-21} #default: 30 minute shows with ads
     min_dur=$(($minutes * 60))
     echo "will write titles >${minutes}m to: $SHOWS_PATH";
     echo "Gettting disc info..."
     HandBrakeCLI -i /dev/sr0 --title=0 --min-duration $min_dur > /tmp/stdout_$MYID.txt 2>/tmp/stderr_$MYID.txt
-    TITLE=`cat /tmp/stdout_$MYID.txt | grep 'DVD Title:' | head -1 | rev | cut -f1 -d":" | rev | xargs`;
     echo "Using Title Prefix $TITLE"    
     if [ -z "$TITLE" ];then
         echo "Error in script"
@@ -35,10 +35,12 @@ function series ()
     TITLES=`cat /tmp/stderr_$MYID.txt | grep -e '+ title [0-9]*' | sed 's/+ title \([0-9]*\):/\1/'`
     TITLES=($TITLES)
     echo "Found ${#TITLES[@]} titles over $minutes minutes (you can change this by passing minues to command)"
-    mkdir "$SHOWS_PATH/${TITLE%_*}"
+    PARENT_DIR="$SHOWS_PATH/${TITLE} (Season ${SEASON})"
+    mkdir "${PARENT_DIR}"
+    EPISODE=$DISC_FIRST_EPISODE
     for index in ${TITLES[@]};do
-        TARG="$SHOWS_PATH/${TITLE%_*}/${TITLE%_*} S${SEASON}E$((${DISC_FIRST_EPISODE}-1+${index})).mp4"
-
+        TARG=`printf "%s/%s - S%02dE%02d.mp4" "${PARENT_DIR}" ${TITLE} ${SEASON} ${EPISODE}`
+ 
         echo "writing title ${index} to  $TARG ...."
         if [ -f "$TARG" ];then
             echo "file exists, skip"
@@ -47,9 +49,8 @@ function series ()
         echo "please wait.."
         HandBrakeCLI --title ${index} -i /dev/sr0 -o "$TARG"  -q 20 -B 160 -s none 2>/tmp/encode_$MYID.txt >&2;
         echo -e "\tdone"  
+        ((EPISODE++))
     done
-    #
-    #echo "wrote movie to: $MP4_PATH/${TITLE}.mp4"
 }
 
 alias mp4='dvd'
